99742cc62c142bc3050b98d157ef2f790da33e15
Filiberka <Filiberka@DESKTOP-E7MVDB9>
2021-06-07 14:24:28 +0200

    Add project files.

---
+++ Controllers/VisitorController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louis_Philippe_Loeckx_PartyMVC.Models
+{
+    public class VisitorController : Controller
+    {
+        //List<Visitor> visitors = new List<Visitor>();
+
+        public IActionResult Input()
+        {
+            TempData["Visitors"] = 0;
+            TempData["FamilyMembers"] = 0;
+            TempData["Oldest"] = 0;
+            TempData["Youngest"] = 0;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Input(Visitor visitor)
+        {
+            TempData["Visitors"] = (Int32)TempData.Peek("Visitors") + 1;
+            if (visitor.IsFamily)
+            {
+                TempData["FamilyMembers"] = (Int32)TempData.Peek("FamilyMembers") + 1;
+            }
+            if ((Int32)TempData.Peek("Youngest") == 0)
+            {
+                TempData["Youngest"] = visitor.Age;
+                TempData["Oldest"] = visitor.Age;
+            }
+            else
+            {
+                if ((Int32)TempData.Peek("Youngest") >= visitor.Age)
+                {
+                    TempData["Youngest"] = visitor.Age;
+                }
+                if ((Int32)TempData.Peek("Oldest") <= visitor.Age)
+                {
+                    TempData["Oldest"] = visitor.Age;
+                }
+            }
+            
+            TempData.Keep();
+            return View(visitor);
+        }
+
+        [HttpGet]
+        public IActionResult Summary()
+        {
+            ViewBag.TotalVisitors = TempData["Visitors"];
+            ViewBag.TotalFamilyMembers = TempData["FamilyMembers"];
+            ViewBag.OldestVisitor = TempData["Oldest"];
+            ViewBag.YoungestVisitor = TempData["Youngest"];
+            return View();
+        }
+    }
+}
---
+++ Models/Visitor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louis_Philippe_Loeckx_PartyMVC.Models
+{
+    public class Visitor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string SecondName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Needs to be valid email")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Tel { get; set; }
+
+        [Required]
+        [Range(18, 60, ErrorMessage = "Age has to be between 18 and 60")]
+
+        public int Age { get; set; }
+        [Required]
+        public string Adress { get; set; }
+
+        [Required]
+        [Range(1000,1500,ErrorMessage ="Postal code has to be between 1000 and 1500")]
+        public int PostalCode { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public bool IsFamily { get; set; }
+
+        public Visitor() 
+        {
+            Id++;
+        }       
+        
+    }
+}
